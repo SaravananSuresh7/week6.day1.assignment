eca4e13442269b55d0074ead7035a74331bc2476
Saravanan Suresh <98247476+SaravananSuresh7@users.noreply.github.com>
2022-03-04 12:30:00 +0530

    Add files via upload

---
+++ Baseclass.java
@@ -0,0 +1,11 @@
+package steps;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+
+
+public class Baseclass extends AbstractTestNGCucumberTests {
+	public static ChromeDriver driver;
+	
+}
